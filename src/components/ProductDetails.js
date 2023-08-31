404afdedeccadd739b1a0f3e3724b475132c345b
Kimutai <jankimsingoei@gmail.com>
2023-08-31 13:23:39 +0300

    feat: Updates on core Components

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+
 function ProductDetails() {
-    const { id } = useParams();
+    const { id } = useParams(); 
     const [selectedPhone, setSelectedPhone] = useState(null);
-    const [deliveryLocation, setDeliveryLocation] = useState({
-        city: "",
-        address: ""
-    });
+    //const [inCart , setInCart] = useState([])
+    // const [deliveryLocation, setDeliveryLocation] = useState({
+    //     city: "",
+    //     address: ""
+    // });
 
     useEffect(() => {
 
@@ -16,64 +18,53 @@ function ProductDetails() {
             .then((data) => setSelectedPhone(data));
     }, [id]);
 
-    const handleAddToCart = () => {
+    function handleAddToCart(phoneData){
+        fetch('http://localhost:8001/cart-details',{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(phoneData)
+        })
+        alert(`${phoneData.name} added to cart`)
+    }
 
-    };
-
-    const handleLocationChange = (e) => {
-        const { name, value } = e.target;
-        setDeliveryLocation((prevLocation) => ({
-            ...prevLocation,
-            [name]: value
-        }));
-    };
+    // const handleLocationChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setDeliveryLocation((prevLocation) => ({
+    //         ...prevLocation,
+    //         [name]: value
+    //     }));
+    // };
 
     if (!selectedPhone) {
-
+    
         return null;
     }
 
     return (
         <div className="prod">
             <div className="product-details">
-                <div className="product-image">
-                    <img src={selectedPhone.img} alt={selectedPhone.name} />
-                </div>
-                <div className="product-info">
-                    <h2>{selectedPhone.name}</h2>
-                    <p>Brand: {selectedPhone.brand}</p>
-                    <p>Price: {selectedPhone.price} USD</p>
-                    <div className="description">
-                        <p>Specifications</p>
-                        <p>Screen Size:{selectedPhone.screen_size}</p>
-                        <p>Release Date:{selectedPhone.release_date}</p>
-                        <p>Camera:{selectedPhone.camera}</p>
-                        <p>Release Date:{selectedPhone.release_date}</p>
-                        <p>Chipset:{selectedPhone.chipset}</p>
-                        <p>Battery:{selectedPhone.battery}</p>
-                        <p>Technology:{selectedPhone.technology}</p>
-                        <p>Sensors:{selectedPhone.sensors}</p>
-                    </div>
-                    <button onClick={handleAddToCart}>Add to Cart</button>
-                </div>
+            <div className="product-image">
+                <img src={selectedPhone.img} alt={selectedPhone.name} />
             </div>
-            <div className="deli">
-                <h3>Delivery Location</h3>
-                <input
-
-                    type="text"
-                    name="city"
-                    placeholder="City"
-                    value={deliveryLocation.city}
-                    onChange={handleLocationChange}
-                />
-                <input
-                    type="text"
-                    name="address"
-                    placeholder="Address"
-                    value={deliveryLocation.address}
-                    onChange={handleLocationChange}
-                />
+            <div className="product-info">
+                <h2>{selectedPhone.name}</h2>
+                <p>Brand: {selectedPhone.brand}</p>
+                <p>Price: {selectedPhone.price} USD</p>
+                <div className="description">
+                <h2>Specifications</h2>
+                <p>Screen Size:{selectedPhone.screen_size}</p>
+                <p>Release Date:{selectedPhone.release_date}</p>
+                <p>Camera:{selectedPhone.camera}</p>
+                <p>Release Date:{selectedPhone.release_date}</p>
+                <p>Chipset:{selectedPhone.chipset}</p>
+                <p>Battery:{selectedPhone.battery}</p>
+                <p>Technology:{selectedPhone.technology}</p>
+                <p>Sensors:{selectedPhone.sensors}</p>
+                </div>
+                <button onClick={() => handleAddToCart(selectedPhone)}>Add to Cart</button>
+            </div>
             </div>
         </div>
     );
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,45 +1,125 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
 
-const ShoppingCart = ({ products }) => {
-  const [selectedProducts, setSelectedProducts] = useState([]);
+function Cart(){
+    const [cart ,setInCart] = useState([])
+    useEffect(() =>{
+        fetch('http://localhost:8001/cart-details')
+        .then((response) => response.json())
+        .then((data) => setInCart(data));
+    },[])
 
-  const handleProductChange = (productId) => {
-    setSelectedProducts((prevSelectedProducts) => {
-      if (prevSelectedProducts.includes(productId)) {
-        return prevSelectedProducts.filter((id) => id !== productId);
-      } else {
-        return [...prevSelectedProducts, productId];
-      }
-    });
-  };
+    function handleDelete(id){
+        console.log(id)
+        fetch(`http://localhost:8001/cart-details/${id}`,{
+            method:"DELETE"
+        })
+        const updatedCart = cart.filter((item)=> item.id !== id)
+        setInCart(updatedCart)
+    }
+    
+    let total = 0
+    let cartData = cart.map((item)=>{
+        let amount = parseFloat(item.price)
+        total+= amount
+    })
+    console.log(cartData)
 
-  const handleCheckout = () => {
-    // You can implement the checkout logic here, e.g. sending data to a backend.
-    console.log('Checkout clicked for products:', selectedProducts);
-  };
+    const [getDate , setDate] = useState('')
+    const [getCardDetails, setCardDetails] = useState('')
+    const [getName, setName]= useState('')
+    const [getAddress , setAddress] = useState('')
 
-  return (
-    <div>
-      <h2>Your Shopping Cart</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <label>
-              <input
-                type="checkbox"
-                checked={selectedProducts.includes(product.id)}
-                onChange={() => handleProductChange(product.id)}
-              />
-              {product.name} - ${product.price}
+    const [getOrder, SetOrder]= useState([])
+
+    function handleDate(e){
+        setDate(e.target.value)
+    }
+    function handleName(e){
+        setName(e.target.value)
+    }
+    function handleCardDetails(e){
+        setCardDetails(e.target.value)
+    }
+    function handleAddress(e){
+        setAddress(e.target.value)
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+         const orderDetails={
+            date:getDate,
+            name:getName,
+            card:getCardDetails,
+            address:getAddress,
+            amount:{total} 
+         }
+         fetch('http://localhost:8001/order-details',{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(orderDetails)
+         })
+         setAddress('')
+         setCardDetails('')
+         setDate('')
+         setName('')
+         alert('order details submitted')
+    }
+    return (
+        <div className="output">
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan ="5"><h3>Cart Details</h3></th>
+                </tr>
+            <tr>
+                <th>Number</th>
+                <th>Item Name</th>
+                <th>Item Brand</th>
+                <th>Amount</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                {cart.map((item , index)=>(
+                    <tr key = {item.id}>
+                        <td>{index}</td>
+                        <td>{item.name}</td>
+                        <td>{item.brand}</td>
+                        <td>{item.price} USD</td>
+                        <td><button onClick ={()=>handleDelete(item.id)}>Remove From Cart</button></td>
+
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <form id = "checkout" onSubmit={handleSubmit}>
+            <h3>Checkout Details</h3>
+            <p>Complete your purchase item by providing your payment details order</p>
+            <label htmlFor="date">
+                Order Date:
+                <input type="date" value ={getDate} onChange={handleDate}/>
             </label>
-          </li>
-        ))}
-      </ul>
-      <button type="button" onClick={handleCheckout}>
-        Checkout
-      </button>
-    </div>
-  );
-};
+            <label htmlFor="name">
+                Customer Name:
+                <input type="text" placeholder="name" value={getName} onChange={handleName}/>
+            </label>
+            <label htmlFor="card">
+                Card Details:
+                <input type="text" placeholder="card details" value={getCardDetails} onChange={handleCardDetails}/>
+            </label>
+            <label>
+                Delivery Address
+                <input type="text"placeholder="address" value={getAddress} onChange={handleAddress}/>
+            </label>
+            <label>
+               Amount to be paid : {total} USDs
+            </label>
+            <button>Proceed To Checkout</button>
+        </form>
 
-export default ShoppingCart;
+        </div>
+    )
+}
+export default Cart;
