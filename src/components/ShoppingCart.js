37d86a4c3f68294107655e34d03b7e8868c7b339
cynthiawuor <cynthiawuor254@gmail.com>
2023-08-31 09:56:44 +0300

    feat: update cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,19 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
-function ProductDetails({ name, category }) {
-  const [isInCart, setIsInCart] =useState (false);
+const ShoppingCart = ({ products }) => {
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
-  const toggleCartStatus = () => {
-    setIsInCart(prevStatus => !prevStatus);
+  const handleProductChange = (productId) => {
+    setSelectedProducts((prevSelectedProducts) => {
+      if (prevSelectedProducts.includes(productId)) {
+        return prevSelectedProducts.filter((id) => id !== productId);
+      } else {
+        return [...prevSelectedProducts, productId];
+      }
+    });
   };
-  return (
-      <li className={isInCart ? 'in-cart' : 'Add to Cart'}>
-        {name}
-      <button onClick={toggleCartStatus}>
-        {isInCart ? 'Remove From Cart' : 'Add to Cart'}
-      </button>
-       </li>
-  );
-}
 
-export default ProductDetails;
+  const handleCheckout = () => {
+    // You can implement the checkout logic here, e.g. sending data to a backend.
+    console.log('Checkout clicked for products:', selectedProducts);
+  };
+
+  return (
+    <div>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={selectedProducts.includes(product.id)}
+                onChange={() => handleProductChange(product.id)}
+              />
+              {product.name} - ${product.price}
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleCheckout}>
+        Checkout
+      </button>
+    </div>
+  );
+};
+
+export default ShoppingCart;
